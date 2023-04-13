38f050592c5af1e443d8222c119d8c33c8bf8526
il_ch <zyhoneec@gmail.com>
2023-04-13 18:46:28 +0300

    added -f to gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,7 @@ const program = new Command;
 program
     .version('1.0.0')
     .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format <type>', 'output usage information')
 ;
 
 program.parse(process.argv);
