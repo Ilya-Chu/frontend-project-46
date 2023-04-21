f16a5eb4707beaadd43a090067ef08f04a4e491b
il_ch <zyhoneec@gmail.com>
2023-04-21 15:12:34 +0600

    added dependencies and test.js

---
+++ src/compareData.js
@@ -0,0 +1,47 @@
+import _ from 'lodash';
+
+const compareData = (obj1, obj2) => {
+    const keys1 = _.keys(obj1);
+    const keys2 = _.keys(obj2);
+    const sortedKeys = _.sortBy(_.union(keys1, keys2));
+
+    const result = sortedKeys.map((key) => {
+        if (!_.has(obj1, key)) {
+            return {
+              key,
+              value: obj2[key],
+              type: 'added',
+            }; 
+          }
+          if (!_.has(obj2, key)) {
+            return {
+              key,
+              value: obj1[key],
+              type: 'deleted',
+            };
+          }
+          if(_.isObject(obj1[key]) && _.isObject(obj2[key])){
+            return {
+              key,
+              type: 'nested',
+              children: compareData(obj1[key], obj2[key]),
+            };
+          }
+          if (obj1[key] !== obj2[key]){
+            return {
+              key,
+              valueBefore: obj1[key],
+              valueAfter: obj2[key],
+              type: 'changed',
+            };
+          }
+          return {
+            key,
+            value: obj1[key],
+            type: 'unchanged'
+          };
+    });
+    return result;
+};
+
+export default compareData;
---
+++ src/parsers.js
@@ -0,0 +1,15 @@
+import yaml from 'js-yaml';
+
+const parse = (data, format) => {
+  switch (format) {
+    case 'json':
+      return JSON.parse(data);
+    case 'yml':
+    case 'yaml':
+      return yaml.load(data);
+    default:
+      throw new Error(`${format} is not supported`);
+  }
+}
+
+export default parse;
